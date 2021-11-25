df56cc05371413da9aca31810f8c779d21f06f43
Saroskami <83586917+Saroskami@users.noreply.github.com>
2021-11-25 09:39:51 +0100

    Add files via upload

---
+++ Roboter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+static void Main(string[] args)
+{
+
+}
+namespace Roboter
+{
+  
+    abstract class Auftrag
+    {
+        public abstract void ausführen();
+    }
+
+    class Roboter
+    {
+        double Ladezustand = 0.0;
+
+        public void ausführen()
+        {
+            Ladezustand = 0.0;
+        }
+
+        public void Laden ()
+        {   if (Ladezustand == 0.0)  Ladezustand = 100.0;
+            Console.WriteLine("Bin vollgeladen!");
+        }
+
+        public void Zustandabfragen()
+        {
+            Console.WriteLine(Ladezustand);
+        }
+    }
+}
